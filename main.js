57a88febe50ec5417ed22b2af589c9021e4ae9f6
Smile_NS <moyohi498@nezumi.be>
2023-06-02 17:27:05 +0900

    clean: to manage the initial settings using "config.json"

--- main.js
+++ main.js
@@ -3,20 +3,21 @@
 const e1 = document.getElementById("join");
 e1.addEventListener('animationend', () => e1.remove());
 
+const url = "./tetris/gamepanel.html";
 const e2 = document.getElementById("math");
-e2.onclick = () => location.href = "./tetris/math/gamepanel.html";
+e2.onclick = () => location.href = `${url}?sbj=math`;
 
 const e3 = document.getElementById("chemistry");
-e3.onclick = () => location.href = "./tetris/chemistry/gamepanel.html";
+e3.onclick = () => location.href = `${url}?sbj=chemistry`;
 
 const e4 = document.getElementById("english");
-e4.onclick = () => location.href = "./tetris/english/gamepanel.html";
+e4.onclick = () => location.href = `${url}?sbj=english`;
 
 const e5 = document.getElementById("geography");
-e5.onclick = () => location.href = "./tetris/geography/gamepanel.html";
+e5.onclick = () => location.href = `${url}?sbj=geography`;
 
 const e6 = document.getElementById("logical_jp");
-e6.onclick = () => location.href = "./tetris/logical_jp/gamepanel.html";
+e6.onclick = () => location.href = `${url}?sbj=logical_jp`;
 
 const e7 = document.getElementById("physics");
-e7.onclick = () => location.href = "./tetris/physics/gamepanel.html";
+e7.onclick = () => location.href = `${url}?sbj=physics`;
